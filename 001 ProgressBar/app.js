190a6a1c530af5cd0378e48eb6a9ca5bed9e24eb
rkhalife <rogerkhalife@outlook.com>
2021-09-06 11:22:25 -0400

    Created Day 1 Project

---
+++ 001 ProgressBar/app.js
@@ -0,0 +1,9 @@
+const progressBar = document.getElementById('progress')
+let number = 0
+const progressTimer = setInterval(incrementProgressBar, 100)
+
+function incrementProgressBar() {
+    number++
+    number == 101 ? clearInterval(progressTimer) : progressBar.value = number
+    console.log(number)
+}
