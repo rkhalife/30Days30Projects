b3503fb1166152367887b4d86a314d80df347889
rkhalife <rogerkhalife@outlook.com>
2021-09-16 13:18:36 -0400

    slight modifications to the code

--- 008 RockPaperScissors/app.js
+++ 008 RockPaperScissors/app.js
@@ -5,41 +5,35 @@ const result = document.querySelector('.result')
 let userSelection = ''
 let cpuChoice = ''
 
-rock.addEventListener('click', () => {
-    userSelection = rock.className
-    cpuRandom()
-})
-
-paper.addEventListener('click', () => {
-    userSelection = 'Paper'
-    cpuRandom()
-})
-
-scissors.addEventListener('click', () => {
-    userSelection = 'Scissors'
-    cpuRandom()
-})
-
-function cpuRandom() {
-    const choices = ['Rock', 'Paper', 'Scissors']
-    const rnd = Math.floor(Math.random() * choices.length)
-    cpuChoice = choices[rnd]
-    result.innerHTML = `You selected ${userSelection}, Computer selected ${cpuChoice}. `
+document.body.addEventListener('click', (e) => {
+    userSelection = e.target.id
+    cpuRandomPick()
+    showResult()
+  })
+  
+  function cpuRandomPick() {
+      const choices = ['Rock', 'Paper', 'Scissors']
+      const rnd = Math.floor(Math.random() * choices.length)
+      cpuChoice = choices[rnd]
+      result.innerHTML = `You selected ${userSelection}, Computer selected ${cpuChoice}. `
+  }
+  
+  function showResult() {
     switch (userSelection) {
-        case cpuChoice:
-            result.innerHTML += "It's a TIE!"
-            break
-        case "Rock":
-            (cpuChoice === "Scissors") ? result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
-            break
-        case "Paper":
-            (cpuChoice === "Rock") ?  result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
-            break
-        case "Scissors":
-            (cpuChoice === "Paper") ? result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
-            break
-        default:
-            result.innerHTML = "Something went wrong"
-    }
-}
-
+          case cpuChoice:
+              result.innerHTML += "It's a TIE!"
+              break
+          case "Rock":
+              (cpuChoice === "Scissors") ? result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
+              break
+          case "Paper":
+              (cpuChoice === "Rock") ?  result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
+              break
+          case "Scissors":
+              (cpuChoice === "Paper") ? result.innerHTML += 'You WON ⭐' : result.innerHTML += 'You LOST 😢'
+              break
+          default:
+              result.innerHTML = "Something went wrong"
+      }
+  }
+  
